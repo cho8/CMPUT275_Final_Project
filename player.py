0fe8108b70160388e6738f3fa312a33c2dde06d6
bsidoruk <bsidoruk@me.com>
2015-03-19 14:35:18 -0600

    changed Player to subclass sprite.Group

--- player.py
+++ player.py
@@ -1,19 +1,21 @@
 from pygame.sprite import Sprite
+from pygame.sprite import Group
 import pygame
 
-class Player(Sprite):
+class Player(Group):
 
     def __init__(self, x_pos=None, y_pos=None, imagefile1=None, imagefile2=None, imagefile3 = None,imagefile4 = None,imagefile5 = None):
 
-        Sprite.__init__(self)        
+        Group.__init__(self)        
 
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.imagefile1 = imagefile1
-        self.imagefile2 = imagefile2
-        self.imagefile3 = imagefile3
-        self.imagefile4 = imagefile4
-        self.imagefile5 = imagefile5
+        self.player = Sprite()
+        self.player.x_pos = x_pos
+        self.player.y_pos = y_pos
+        self.player.imagefile1 = imagefile1
+        self.player.imagefile2 = imagefile2
+        self.player.imagefile3 = imagefile3
+        self.player.imagefile4 = imagefile4
+        self.player.imagefile5 = imagefile5
      
 
         #Put initial attributes here
@@ -22,50 +24,51 @@ class Player(Sprite):
 
 
         #
-        self.image1 = pygame.image.load(imagefile1).convert_alpha()
-        self.image2 = pygame.image.load(imagefile2).convert_alpha()
-        self.image3 = pygame.image.load(imagefile3).convert_alpha()
-        self.image4 = pygame.image.load(imagefile4).convert_alpha()
-        self.image5 = pygame.image.load(imagefile5).convert_alpha()
-        self.image = self.image1
-        self.rect = self.image.get_rect()
-        self.rect.x = 20
-        self.rect.y = 20
+        self.player.image1 = pygame.image.load(imagefile1).convert_alpha()
+        self.player.image2 = pygame.image.load(imagefile2).convert_alpha()
+        self.player.image3 = pygame.image.load(imagefile3).convert_alpha()
+        self.player.image4 = pygame.image.load(imagefile4).convert_alpha()
+        self.player.image5 = pygame.image.load(imagefile5).convert_alpha()
+        self.player.image = self.player.image1
+        self.player.rect = self.player.image.get_rect()
+        self.player.rect.x = x_pos
+        self.player.rect.y = x_pos
+        self.add(self.player)
         print("reset")
 
-    def move(self,direction,group,buildings,screen,background):
+    def move(self,direction,buildings,screen,background):
         speed = 2
         if direction == "LEFT" or direction == "RIGHT":
             if direction == "LEFT":
                 speed *= -1
-                self.image = self.image4
+                self.player.image = self.player.image4
             else:
-                self.image = self.image5
-            if pygame.sprite.spritecollideany(self,buildings) != None:
+                self.player.image = self.player.image5
+            if pygame.sprite.spritecollideany(self.player,buildings) != None:
                 if direction == "LEFT":
-                    self.rect.x += 1
+                    self.player.rect.x += 1
                 else:
-                    self.rect.x -= 1
+                    self.player.rect.x -= 1
             else:
-                self.rect.x += speed
+                self.player.rect.x += speed
             
         elif direction == "UP" or direction == "DOWN":
             if direction == "UP":
                 speed *= -1
-                self.image = self.image3
+                self.player.image = self.player.image3
             else:
-                self.image = self.image1
-            if pygame.sprite.spritecollideany(self,buildings) != None:
+                self.player.image = self.player.image1
+            if pygame.sprite.spritecollideany(self.player,buildings) != None:
                 if direction == "UP":
-                    self.rect.y += 1
+                    self.player.rect.y += 1
                 else:
-                    self.rect.y -= 1
+                    self.player.rect.y -= 1
             else:
-                self.rect.y += speed
+                self.player.rect.y += speed
 
         
-        group.clear(screen,background)
-        group.draw(screen)
-        pygame.display.update(self.rect)
+        self.clear(screen,background)
+        self.draw(screen)
+        pygame.display.update(self.player.rect)
 
 
