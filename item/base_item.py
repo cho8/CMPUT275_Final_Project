d1dc0d4ade88b21b299d89f5d16666e4665fc31c
Christina Ho <c.ho784@gmail.com>
2015-03-20 10:35:07 -0600

    added base_item.py

--- item/base_item.py
+++ item/base_item.py
@@ -2,3 +2,15 @@ import pygame, item
 from pygame.sprite import Sprite
 
 size = 20
+
+class BaseItem(Sprite):
+    """
+    Basic representation of items that can be found around the playable area.
+    Includes sprites, stat changes and effects.
+    """
+
+    def __init__(self):
+        self.name = "Base Item"
+        self.description = "Base Description"
+
+        self.
